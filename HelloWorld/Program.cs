50bd560aebb292975504967d4badeede6989fc32
Abhishek32971 <abhishekrp32971@gmail.com>
2024-06-12 13:38:59 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Convert.ToInt32("1234");
+            int j = int.Parse("1234");
+        }
+    }
+}
